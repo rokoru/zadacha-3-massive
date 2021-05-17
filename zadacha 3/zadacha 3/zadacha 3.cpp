d74722bcc18735223f5d0e5264b0e8806384ea92
Dmitry <gomerty15@mail.ru>
2021-05-17 14:53:57 +0300

    добавленно решение

---
+++ zadacha 3/zadacha 3/zadacha 3.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+    int* a = new int[50];
+    int* b = new int[50];
+
+    for (int i = 0; i < 50; ++i) {
+        a[i] = rand() % 30;
+    }
+
+    for (int i = 0; i < 50; ++i) {
+        b[i] = rand() % 50;
+    }
+
+    int* c = new int[50];
+
+    for (int i = 0; i < 50; ++i) {
+        c[i] = a[i] + b[i];
+        std::cout << c[i] << std::endl;
+    }
+}
+
